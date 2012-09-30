c0e94918f79ff96e20f323a51119719c4a5bc610
Laura <laura-toshiba@laura-toshiba-Satellite-U305.(none)>
2012-09-29 20:30:17 -0500

    Adding activerecord

--- minimal.rb
+++ minimal.rb
@@ -1,5 +1,18 @@
 require 'rubygems'
-require 'sinatra';
+require 'sinatra'
+require 'active_record';
+
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :port     => db.port,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
+)
 
 get '/' do
   haml :index
